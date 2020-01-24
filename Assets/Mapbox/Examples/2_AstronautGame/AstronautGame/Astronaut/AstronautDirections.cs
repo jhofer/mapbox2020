2c426db9d8f5c0430af0179ca8af2c4d9f16e907
Jonas Hofer <jonas.hofer@outlook.com>
2020-01-24 08:22:30 +0100

    inital commit

---
+++ Assets/Mapbox/Examples/2_AstronautGame/AstronautGame/Astronaut/AstronautDirections.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using Mapbox.Directions;
+using System.Collections.Generic;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Data;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+using Mapbox.Unity;
+using System;
+
+namespace Mapbox.Examples
+{
+    // TODO: move to own file
+    public enum UnitType
+    {
+        Vehicle,
+        Soldier,
+    }
+
+	public class AstronautDirections : MonoBehaviour
+	{
+		AbstractMap _map;
+		Directions.Directions _directions;
+		Action<List<Vector3>> callback;
+        RoutingProfile routingProfile;
+
+        [SerializeField]
+        UnitType unitType;
+
+        void Awake()
+		{
+			_directions = MapboxAccess.Instance.Directions;
+            switch (unitType)
+            {
+                case UnitType.Soldier:
+                    routingProfile = RoutingProfile.Walking;
+                    break;
+                case UnitType.Vehicle:
+                    routingProfile = RoutingProfile.Driving;
+                    break;
+                default:
+                    routingProfile = RoutingProfile.Driving;
+                    break;
+            }
+        }
+
+		public void Query(Action<List<Vector3>> vecs, Transform start, Transform end, AbstractMap map)
+		{
+			if (callback == null)
+				callback = vecs;
+
+			_map = map;
+
+			var wp = new Vector2d[2];
+			wp[0] = start.GetGeoPosition(_map.CenterMercator, _map.WorldRelativeScale);
+			wp[1] = end.GetGeoPosition(_map.CenterMercator, _map.WorldRelativeScale);
+			var _directionResource = new DirectionResource(wp, routingProfile);
+			_directionResource.Steps = true;
+			_directions.Query(_directionResource, HandleDirectionsResponse);
+		}
+
+		void HandleDirectionsResponse(DirectionsResponse response)
+		{
+			if (null == response.Routes || response.Routes.Count < 1)
+			{
+				return;
+			}
+
+			var dat = new List<Vector3>();
+			foreach (var point in response.Routes[0].Geometry)
+			{
+				dat.Add(Conversions.GeoToWorldPosition(point.x, point.y, _map.CenterMercator, _map.WorldRelativeScale).ToVector3xz());
+			}
+
+			callback(dat);
+		}
+	}
+}
